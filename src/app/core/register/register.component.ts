0ae148a9407821542a80777d7035c431a5ccdbfe
DerGiftzwerg156 <50719189+DerGiftzwerg156@users.noreply.github.com>
2022-11-18 23:34:39 +0100

    Ticket #4
    Added Messager to comparePasswords by register new User

--- src/app/core/register/register.component.ts
+++ src/app/core/register/register.component.ts
@@ -1,29 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthService} from "../../../services/auth.service";
 import {RegisterDataRequest} from "../../../requestTypes/RegisterDataRequest";
+import {MessageService, PrimeNGConfig} from "primeng/api";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  providers: [MessageService]
 })
 export class RegisterComponent implements OnInit {
   password: any;
   mail: any;
   lastname: any;
   firstName: any;
+  passwordConfirm?: string;
+  samePassword?: boolean;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private messageService: MessageService, private primengConfig: PrimeNGConfig) {
+  }
 
   ngOnInit(): void {
   }
 
   register() {
-    this.authService.register(new RegisterDataRequest(this.firstName, this.lastname, this.mail, this.password));
+    this.primengConfig.ripple = true;
+    if (this.comparePasswords()) {
+      this.authService.register(new RegisterDataRequest(this.firstName, this.lastname, this.mail, this.password));
+    }
   }
 
   navigateToLogin() {
     this.router.navigate(['/login']);
+  }
+
+  showErrorMessage(summary: string, message: string) {
+    this.messageService.add({key: 'registerMessager', severity: 'error', summary: summary, detail: message});
+  }
+
+  comparePasswords() {
+    console.log(this.password === this.passwordConfirm)
+    if (this.password === this.passwordConfirm) {
+      return true;
+    } else {
+      this.showErrorMessage("Fehler", "Die Passwörter stimmern nicht überein.");
+      return false;
+    }
   }
 }
