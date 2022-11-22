d75c78150e52a072a12f13b786b827b8c44c2348
DerGiftzwerg156 <50719189+DerGiftzwerg156@users.noreply.github.com>
2022-11-22 23:06:51 +0100

    Changed Urls

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -14,8 +14,8 @@ import {ActivateAccountRequest} from "../requestTypes/ActivateAccountRequest";
 })
 export class AuthService {
 
-  // authUrl: string = window.location.origin+"/api/auth"
-  authUrl: string = "http://localhost:8080/api/auth"
+  authUrl: string = window.location.origin+"/api/auth"
+  // authUrl: string = "http://localhost:8080/api/auth"
 
   constructor(private http: HttpClient, private router: Router, private logger: LoggerService) {
   }
